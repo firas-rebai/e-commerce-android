d8952188512e3346815193839e756f44f991b934
yosrasejjil <ysejjil@gmail.com>
2021-12-06 13:58:50 +0100

    firebase added

--- app/src/main/java/com/example/controller/Login.java
+++ app/src/main/java/com/example/controller/Login.java
@@ -2,15 +2,31 @@ package com.example.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 
-
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements View.OnClickListener {
+    private TextView register ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        register=(TextView) findViewById(R.id.register);
+        register.setOnClickListener(this);
     }
-}
+
+
+    @Override
+    public void onClick(View v) { switch (v.getId()){
+            case R.id.register:
+                startActivity(new Intent(this,register.getClass()));
+                break;
+        }
+        }
+
+    }
---
+++ app/src/main/java/com/example/controller/register.java
@@ -0,0 +1,20 @@
+package com.example.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class register extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+}
+
